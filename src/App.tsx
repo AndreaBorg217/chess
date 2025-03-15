e7d3c42834c6e5e8a10ccea952fca0d6d6104e21
Andrea Borg <andreaborg217@gmail.com>
2025-03-15 14:07:03 +0100

    quality: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-import Piece from './models/piece'
 import GameController from './controller'
-import { Colour } from './constants'
-import {GameState} from './constants'
 import PawnSwapModal from './components/pawn_swap_modal';
 import Board from './components/board';
 import { Stats } from './components/stats/stats';
